bd5c9d340a817df88fc70df83b3f54917dbe7595
Luca Antiga <luca.antiga@orobix.com>
2016-09-18 01:18:18 +0200

    Completed tensor and run API.

---
+++ models/load_model.sh
@@ -0,0 +1,6 @@
+../deps/redis/src/redis-cli -x SET foo < graph.pb
+../deps/redis/src/redis-cli TF.TENSOR a UINT8 1 1 1
+../deps/redis/src/redis-cli TF.TENSOR b UINT8 1 1 1
+../deps/redis/src/redis-cli TF.RUN foo 2 a a b b c c
+../deps/redis/src/redis-cli --raw TF.DATA c
+
