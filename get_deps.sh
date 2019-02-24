f570f9b55d8ded0e91e1d691e6ea9d3c2f154e9e
Mark Nunberg <mnunberg@haskalah.org>
2019-02-24 07:51:00 -0500

    Further clean up dependency handling

--- get_deps.sh
+++ get_deps.sh
@@ -14,10 +14,7 @@ DEPS_DIRECTORY=${BASE_DIRECTORY}/deps-${PLATNAME}
 
 mkdir -p ${DEPS_DIRECTORY}
 
-## DLPACK
-
 cd ${DEPS_DIRECTORY}
-
 DLPACK_DIRECTORY=${DEPS_DIRECTORY}/dlpack
 
 if [ ! -d dlpack ]; then
@@ -25,21 +22,8 @@ if [ ! -d dlpack ]; then
     git clone --depth 1 https://github.com/dmlc/dlpack.git
 fi
 
-## REDIS
-
-cd ${DEPS_DIRECTORY}
-
-if [ ! -d redis ]; then
-    echo "Building Redis"
-    git clone --depth 1 --branch 5.0 https://github.com/antirez/redis.git
-    cd ${DEPS_DIRECTORY}/redis
-    make MALLOC=libc
-fi
-
 ## TENSORFLOW
-
 cd ${DEPS_DIRECTORY}
-
 TF_VERSION="1.12.0"
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   TF_OS="linux"
@@ -53,20 +37,20 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   TF_BUILD="cpu"
 fi
 
-LIBTF_DIRECTORY=${DEPS_DIRECTORY}/libtensorflow
+LIBTF_ARCHIVE=libtensorflow-${TF_BUILD}-${TF_OS}-x86_64-${TF_VERSION}.tar.gz
 
-if [ ! -d "${LIBTF_DIRECTORY}" ]; then
-  # rm -rf ${LIBTF_DIRECTORY}
+if [ ! -e ${LIBTF_ARCHIVE} ]; then
   echo "Downloading libtensorflow ${TF_VERSION} ${TF_BUILD}"
-  mkdir -p ${LIBTF_DIRECTORY}
-  curl -L \
-    "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_BUILD}-${TF_OS}-x86_64-${TF_VERSION}.tar.gz" |
-    tar -C ${LIBTF_DIRECTORY} -xz
+  wget https://storage.googleapis.com/tensorflow/libtensorflow/${LIBTF_ARCHIVE}
+fi
+
+LIBTF_DIRECTORY=${DEPS_DIRECTORY}/libtensorflow
+if [ ! -d ${LIBTF_DIRECTORY} ]; then
+  mkdir ${LIBTF_DIRECTORY}
+  tar xf ${LIBTF_ARCHIVE} -C ${LIBTF_DIRECTORY}
 fi
 
 ## PYTORCH
-
-cd ${DEPS_DIRECTORY}
 
 PT_VERSION="1.0.1"
 #PT_VERSION="latest"
@@ -87,39 +71,31 @@ if [[ "$PT_VERSION" == "latest" ]]; then
   PT_BUILD=nightly/${PT_BUILD}
 fi
 
-LIBTORCH_DIRECTORY=`pwd`/libtorch
+# Where to get the archive
+LIBTORCH_URL=https://download.pytorch.org/libtorch/${PT_BUILD}/libtorch-${PT_OS}-${PT_VERSION}.zip
+# Directory where torch is extracted to
+LIBTORCH_DIRECTORY=libtorch-${PT_OS}-${PT_VERSION}
+# Archive - specifically named
+LIBTORCH_ARCHIVE=libtorch-${PT_OS}-${PT_BUILD}-${PT_VERSION}.zip
 
-if [ ! -d "${LIBTORCH_DIRECTORY}" ]; then
+if [ ! -e "${LIBTORCH_ARCHIVE}" ]; then
   echo "Downloading libtorch ${PT_VERSION} ${PT_BUILD}"
-  # rm -rf ${LIBTORCH_DIRECTORY}
-  curl -o tmp.zip \
-    "https://download.pytorch.org/libtorch/${PT_BUILD}/libtorch-${PT_OS}-${PT_VERSION}.zip" && \
-    unzip tmp.zip && rm tmp.zip
-  if [[ "$PT_OS" == "macos" ]]; then
-    mkdir -p ./mkl_tmp
-    MKL_BUNDLE=mklml_mac_2019.0.1.20180928
-    curl -L \
-      "https://github.com/intel/mkl-dnn/releases/download/v0.17.1/${MKL_BUNDLE}.tgz" |
-      tar -C ./mkl_tmp -xz
-    mv mkl_tmp/${MKL_BUNDLE}/license.txt ${LIBTORCH_DIRECTORY}/mkl_license.txt
-    mv mkl_tmp/${MKL_BUNDLE}/lib/* ${LIBTORCH_DIRECTORY}/lib
-    mkdir -p ${LIBTORCH_DIRECTORY}/include/mkl
-    mv mkl_tmp/${MKL_BUNDLE}/include/* ${LIBTORCH_DIRECTORY}/include/mkl
-    rm -rf mkl_tmp
-  fi
+  curl -L ${LIBTORCH_URL} > ${LIBTORCH_ARCHIVE}
 fi
 
-## INSTALL
+rm -rf ${LIBTORCH_DIRECTORY}
+unzip -o ${LIBTORCH_ARCHIVE}
 
-DEPS_INSTALL_LIB_DIRECTORY=${DEPS_DIRECTORY}/install/lib
-mkdir -p ${DEPS_INSTALL_LIB_DIRECTORY}
-
-cp -R ${DEPS_DIRECTORY}/libtensorflow/lib/* ${DEPS_INSTALL_LIB_DIRECTORY}
-cp -R ${DEPS_DIRECTORY}/libtorch/lib/* ${DEPS_INSTALL_LIB_DIRECTORY}
-cp -R ${DEPS_DIRECTORY}/libtorch_c/lib/* ${DEPS_INSTALL_LIB_DIRECTORY}
-
-## DONE
-
-cd ${BASE_DIRECTORY}
+if [[ "${PT_OS}" == "macos" ]]; then
+  # also download mkl
+  MKL_BUNDLE=mklml_mac_2019.0.1.20180928
+  if [ ! -e "${MKL_BUNDLE}.tgz" ]; then
+    wget "https://github.com/intel/mkl-dnn/releases/download/v0.17.1/${MKL_BUNDLE}.tgz"
+  fi
+  rm -rf ${MKL_BUNDLE}
+  tar xf ${MKL_BUNDLE}.tgz
+  rm -f mkl
+  ln -s ${MKL_BUNDLE} mkl
+fi
 
 echo "Done"
