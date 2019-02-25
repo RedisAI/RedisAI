ca63a67faad9c0eda048139ee9a141f588e4d303
Mark Nunberg <mnunberg@users.noreply.github.com>
2019-02-25 17:37:50 -0500

    Build refactor (#65)
    * get_deps: add -e, allow per-platform directory
    
    -e ensures that the retrieval fails at first error. Multi-platform
    builds are useful in case the directory is moved across different
    virtual platforms
    
    * Use CMake build instead of normal Makefiles
    
    * add residmodule.h
    
    * Further clean up dependency handling
    
    * more build fixes...
    
    * add test_requirements.txt
    
    * Fix test usage to be more in line with rltest APIs
    
    This eliminates the need for a custom rltest version
    
    * Fixes for CI
    
    * Set flags to C99

--- get_deps.sh
+++ get_deps.sh
@@ -1,35 +1,38 @@
 #!/usr/bin/env bash
+set -e
+set -x
 
 BASE_DIRECTORY=`pwd`
 
-DEPS_DIRECTORY=${BASE_DIRECTORY}/deps
+# Allow different deps for different platforms:
+PLATNAME=${OSTYPE}
+if [ -e /etc/debian-version ]; then
+    PLATNAME=${PLATNAME}-deb
+fi
+
+if [ -z "$DEPS_DIRECTORY" ]; then
+  DEPS_DIRECTORY=${BASE_DIRECTORY}/deps-${PLATNAME}
+fi
 
 mkdir -p ${DEPS_DIRECTORY}
-
-## DLPACK
-
 cd ${DEPS_DIRECTORY}
+DEPS_DIRECTORY=$PWD # -- to avoid relative/absolute confusion
 
-DLPACK_DIRECTORY=${DEPS_DIRECTORY}/dlpack
+PREFIX=${DEPS_DIRECTORY}/install
+mkdir -p ${PREFIX}
+rm -rf ${PREFIX}/share
+rm -rf ${PREFIX}/lib
+rm -rf ${PREFIX}/include
 
-echo "Cloning dlpack"
-git clone --depth 1 https://github.com/dmlc/dlpack.git
+if [ ! -d dlpack ]; then
+    echo "Cloning dlpack"
+    git clone --depth 1 https://github.com/dmlc/dlpack.git
+fi
 
-## REDIS
-
-cd ${DEPS_DIRECTORY}
-
-echo "Building Redis"
-git clone --depth 1 --branch 5.0 https://github.com/antirez/redis.git
-
-cd ${DEPS_DIRECTORY}/redis
-
-make MALLOC=libc
+mkdir -p ${PREFIX}
+cp -a dlpack/include ${PREFIX}
 
 ## TENSORFLOW
-
-cd ${DEPS_DIRECTORY}
-
 TF_VERSION="1.12.0"
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   TF_OS="linux"
@@ -43,20 +46,16 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   TF_BUILD="cpu"
 fi
 
-LIBTF_DIRECTORY=`pwd`/libtensorflow
+LIBTF_ARCHIVE=libtensorflow-${TF_BUILD}-${TF_OS}-x86_64-${TF_VERSION}.tar.gz
 
-if [ ! -d "${LIBTF_DIRECTORY}" ]; then
-  # rm -rf ${LIBTF_DIRECTORY}
+if [ ! -e ${LIBTF_ARCHIVE} ]; then
   echo "Downloading libtensorflow ${TF_VERSION} ${TF_BUILD}"
-  mkdir -p ${LIBTF_DIRECTORY}
-  curl -L \
-    "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_BUILD}-${TF_OS}-x86_64-${TF_VERSION}.tar.gz" |
-    tar -C ${LIBTF_DIRECTORY} -xz
+  wget https://storage.googleapis.com/tensorflow/libtensorflow/${LIBTF_ARCHIVE}
 fi
 
-## PYTORCH
+tar xf ${LIBTF_ARCHIVE} --strip-components=1 -C ${PREFIX}
 
-cd ${DEPS_DIRECTORY}
+## PYTORCH
 
 PT_VERSION="1.0.1"
 #PT_VERSION="latest"
@@ -77,89 +76,30 @@ if [[ "$PT_VERSION" == "latest" ]]; then
   PT_BUILD=nightly/${PT_BUILD}
 fi
 
-LIBTORCH_DIRECTORY=`pwd`/libtorch
+# Where to get the archive
+LIBTORCH_URL=https://download.pytorch.org/libtorch/${PT_BUILD}/libtorch-${PT_OS}-${PT_VERSION}.zip
+# Directory where torch is extracted to
+LIBTORCH_DIRECTORY=libtorch-${PT_OS}-${PT_VERSION}
 
-if [ ! -d "${LIBTORCH_DIRECTORY}" ]; then
+# Archive - specifically named
+LIBTORCH_ARCHIVE=libtorch-${PT_OS}-${PT_BUILD}-${PT_VERSION}.zip
+
+if [ ! -e "${LIBTORCH_ARCHIVE}" ]; then
   echo "Downloading libtorch ${PT_VERSION} ${PT_BUILD}"
-  # rm -rf ${LIBTORCH_DIRECTORY}
-  curl -o tmp.zip \
-    "https://download.pytorch.org/libtorch/${PT_BUILD}/libtorch-${PT_OS}-${PT_VERSION}.zip" && \
-    unzip tmp.zip && rm tmp.zip
-  if [[ "$PT_OS" == "macos" ]]; then
-    mkdir -p ./mkl_tmp
-    MKL_BUNDLE=mklml_mac_2019.0.1.20180928
-    curl -L \
-      "https://github.com/intel/mkl-dnn/releases/download/v0.17.1/${MKL_BUNDLE}.tgz" |
-      tar -C ./mkl_tmp -xz
-    mv mkl_tmp/${MKL_BUNDLE}/license.txt ${LIBTORCH_DIRECTORY}/mkl_license.txt
-    mv mkl_tmp/${MKL_BUNDLE}/lib/* ${LIBTORCH_DIRECTORY}/lib
-    mkdir -p ${LIBTORCH_DIRECTORY}/include/mkl
-    mv mkl_tmp/${MKL_BUNDLE}/include/* ${LIBTORCH_DIRECTORY}/include/mkl
-    rm -rf mkl_tmp
+  curl -L ${LIBTORCH_URL} > ${LIBTORCH_ARCHIVE}
+fi
+
+unzip -o ${LIBTORCH_ARCHIVE}
+tar cf - libtorch | tar xf - --strip-components=1 -C ${PREFIX}
+rm -rf libtorch
+
+if [[ "${PT_OS}" == "macos" ]]; then
+  # also download mkl
+  MKL_BUNDLE=mklml_mac_2019.0.1.20180928
+  if [ ! -e "${MKL_BUNDLE}.tgz" ]; then
+    wget "https://github.com/intel/mkl-dnn/releases/download/v0.17.1/${MKL_BUNDLE}.tgz"
   fi
+  tar xf ${MKL_BUNDLE}.tgz --strip-components=1 -C ${PREFIX}
 fi
-
-## UTIL
-
-cd ${DEPS_DIRECTORY}
-
-LIBTORCH_C_SRC_DIRECTORY=${BASE_DIRECTORY}/util/libtorch_c
-LIBTORCH_C_DIRECTORY=${DEPS_DIRECTORY}/libtorch_c
-
-mkdir -p ${LIBTORCH_C_DIRECTORY}/build
-cd ${LIBTORCH_C_DIRECTORY}/build
-
-cmake -DCMAKE_PREFIX_PATH=${LIBTORCH_DIRECTORY} \
-      -DDLPACK_DIRECTORY=${DLPACK_DIRECTORY} \
-      -DCMAKE_INSTALL_PREFIX=${LIBTORCH_C_DIRECTORY} \
-      ${LIBTORCH_C_SRC_DIRECTORY}
-make -j2 && make install
-
-cd ${LIBTORCH_C_DIRECTORY}
-rm -rf ${LIBTORCH_C_DIRECTORY}/build
-
-## INSTALL
-
-DEPS_INSTALL_LIB_DIRECTORY=${DEPS_DIRECTORY}/install/lib
-
-mkdir -p ${DEPS_INSTALL_LIB_DIRECTORY}
-
-cp -R ${DEPS_DIRECTORY}/libtensorflow/lib/* ${DEPS_INSTALL_LIB_DIRECTORY}
-cp -R ${DEPS_DIRECTORY}/libtorch/lib/* ${DEPS_INSTALL_LIB_DIRECTORY}
-cp -R ${DEPS_DIRECTORY}/libtorch_c/lib/* ${DEPS_INSTALL_LIB_DIRECTORY}
-
-## TEST
-
-cd ${BASE_DIRECTORY}/test
-
-echo "Testing TensorFlow"
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${LIBTF_DIRECTORY}/lib
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:${LIBTF_DIRECTORY}/lib
-fi
-gcc -I${LIBTF_DIRECTORY}/include \
-    -L${LIBTF_DIRECTORY}/lib \
-    tf_api_test.c -ltensorflow && \
-    ./a.out && rm a.out
-
-echo "Testing PyTorch"
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${LIBTORCH_DIRECTORY}/lib:${LIBTORCH_C_DIRECTORY}/lib
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:${LIBTORCH_DIRECTORY}/lib:${LIBTORCH_C_DIRECTORY}/lib
-fi
-gcc -I${LIBTORCH_C_DIRECTORY}/include \
-    -I${DLPACK_DIRECTORY}/include \
-    -L${LIBTORCH_C_DIRECTORY}/lib \
-    pt_api_test.c -ltorch_c && \
-    ./a.out && \
-    rm a.out
-
-## DONE
-
-cd ${BASE_DIRECTORY}
 
 echo "Done"
