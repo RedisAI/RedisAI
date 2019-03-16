57a8861c20410181d5c92f5dbe3be9877cfe815b
Meir Shpilraien <meir@redislabs.com>
2019-03-17 01:21:18 +0200

    Small api changes:
    1. add REDISAI_LLAPI_VERSION and RedisAI_GetAPIVersion to check api version.
    2. change RAI_Backend and RAI_Device from enum to define so it will be possible to use them from the redisai.h only.

--- src/redisai.h
+++ src/redisai.h
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include "redismodule.h"
 
+#define REDISAI_LLAPI_VERSION 1
+
 #define MODULE_API_FUNC(x) (*x)
 
 typedef struct RAI_Tensor RAI_Tensor;
@@ -13,8 +15,12 @@ typedef struct RAI_Script RAI_Script;
 typedef struct RAI_ModelRunCtx RAI_ModelRunCtx;
 typedef struct RAI_ScriptRunCtx RAI_ScriptRunCtx;
 
-typedef enum RAI_Backend RAI_Backend;
-typedef enum RAI_Device RAI_Device;
+#define REDISAI_BACKEND_TENSORFLOW 0
+#define REDISAI_BACKEND_TORCH 1
+#define REDISAI_BACKEND_ONNXRUNTIME 2
+
+#define REDISAI_DEVICE_CPU 0
+#define REDISAI_DEVICE_GPU 1
 
 RAI_Tensor* MODULE_API_FUNC(RedisAI_TensorCreate)(const char* dataTypeStr, long long* dims, int ndims);
 size_t MODULE_API_FUNC(RedisAI_TensorLength)(RAI_Tensor* t);
@@ -32,7 +38,7 @@ long long MODULE_API_FUNC(RedisAI_TensorDim)(RAI_Tensor* t, int dim);
 size_t MODULE_API_FUNC(RedisAI_TensorByteSize)(RAI_Tensor* t);
 char* MODULE_API_FUNC(RedisAI_TensorData)(RAI_Tensor* t);
 
-RAI_Model* MODULE_API_FUNC(RedisAI_ModelCreate)(RAI_Backend backend, RAI_Device device, const char* modeldef, size_t modellen);
+RAI_Model* MODULE_API_FUNC(RedisAI_ModelCreate)(int backend, int device, const char* modeldef, size_t modellen);
 void MODULE_API_FUNC(RedisAI_ModelFree)(RAI_Model* model);
 RAI_ModelRunCtx* MODULE_API_FUNC(RedisAI_ModelRunCtxCreate)(RAI_Model* model);
 int MODULE_API_FUNC(RedisAI_ModelRunCtxAddInput)(RAI_ModelRunCtx* mctx, const char* inputName, RAI_Tensor* inputTensor);
@@ -43,7 +49,7 @@ void MODULE_API_FUNC(RedisAI_ModelRunCtxFree)(RAI_ModelRunCtx* mctx);
 int MODULE_API_FUNC(RedisAI_ModelRun)(RAI_ModelRunCtx* mctx);
 RAI_Model* MODULE_API_FUNC(RedisAI_ModelGetShallowCopy)(RAI_Model* model);
 
-RAI_Script* MODULE_API_FUNC(RedisAI_ScriptCreate)(RAI_Backend backend, RAI_Device device, const char* modeldef, size_t modellen);
+RAI_Script* MODULE_API_FUNC(RedisAI_ScriptCreate)(int backend, int device, const char* modeldef, size_t modellen);
 void MODULE_API_FUNC(RedisAI_ScriptFree)(RAI_Script* model);
 RAI_ScriptRunCtx* MODULE_API_FUNC(RedisAI_ScriptRunCtxCreate)(RAI_Script* model);
 int MODULE_API_FUNC(RedisAI_ScriptRunCtxAddInput)(RAI_ScriptRunCtx* mctx, const char* inputName, RAI_Tensor* inputTensor);
@@ -54,16 +60,18 @@ void MODULE_API_FUNC(RedisAI_ScriptRunCtxFree)(RAI_ScriptRunCtx* mctx);
 int MODULE_API_FUNC(RedisAI_ScriptRun)(RAI_ScriptRunCtx* mctx);
 RAI_Script* MODULE_API_FUNC(RedisAI_ScriptGetShallowCopy)(RAI_Script* model);
 
+int MODULE_API_FUNC(RedisAI_GetAPIVersion)();
+
 
 #define REDISAI_MODULE_INIT_FUNCTION(name) \
   if (RedisModule_GetApi("RedisAI_" #name, ((void **)&RedisAI_ ## name))) { \
     printf("could not initialize RedisAI_" #name "\r\n");\
-    return false; \
+    return REDISMODULE_ERR; \
   }
 
-static bool RediDL_Initialize(){
+static int RediAI_Initialize(){
 
-  RMUTil_InitAlloc();
+  REDISAI_MODULE_INIT_FUNCTION(GetAPIVersion);
 
   REDISAI_MODULE_INIT_FUNCTION(TensorCreate);
   REDISAI_MODULE_INIT_FUNCTION(TensorGetDataSize);
@@ -101,7 +109,11 @@ static bool RediDL_Initialize(){
   REDISAI_MODULE_INIT_FUNCTION(ScriptRun);
   REDISAI_MODULE_INIT_FUNCTION(ScriptGetShallowCopy);
 
-  return true;
+  if(RedisAI_GetAPIVersion() < REDISAI_LLAPI_VERSION){
+    return REDISMODULE_ERR;
+  }
+
+  return REDISMODULE_OK;
 }
 
 #endif /* SRC_REDISAI_H_ */
