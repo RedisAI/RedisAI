e48a479729d834d3eebfd9e63f1b9ccdb150b055
Sherin Thomas <sherin@tensorwerk.com>
2019-07-02 17:07:17 +0530

    Cleanup examples (#153)
    * example folder is gone and moved required artifacts to test folder
    
    * made it work with new path
    
    * README updates

--- examples/models/graph2.pb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7ecdc7bc541c17743d54576c48186c702a623657e2a604b300f9bf2b70d15af2
-size 360
--- test/test_data/pt_minimal.py
+++ test/test_data/pt_minimal.py
@@ -1,17 +1,5 @@
 import torch
 
-#def bar(a, b):
-#    return a + b
-#
-#m = torch.jit.trace(bar, (torch.rand(2), torch.rand(2)))
-#
-#torch.jit.save(m, 'pt-minimal.pt')
-#
-#m2 = torch.jit.load('pt-minimal.pt')
-#
-#out = m2.forward(torch.rand(2), torch.rand(2))
-#print(out)
-
 
 class MyModule(torch.jit.ScriptModule):
     def __init__(self):
@@ -21,8 +9,7 @@ class MyModule(torch.jit.ScriptModule):
     def forward(self, a, b):
         return a + b
 
+
 my_script_module = MyModule()
-
 print(my_script_module(torch.rand(2), torch.rand(2)))
-
 my_script_module.save("foo.pt")
