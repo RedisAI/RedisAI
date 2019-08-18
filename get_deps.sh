749d7582dc31c997095b4995263d8516c57f48f3
Rafi Einstein <rafi@redislabs.com>
2019-08-18 14:51:40 +0300

    Makefile: added pack and test targets (#169)
    * Makefile: added pack and test targets
    
    * Added deps/readies and system-setup.py
    
    * Fixed docker functionality, fixed tests, setup changes
    
    - Added deps/readies and system-setup.py to provide standard system setup,
    - Docker: load all backends,
    - Tests: load backends upfront to avoid rdb problems,
    - Removed Python virtual environment setup in Makefile to avoid setup
      nightmares in various platforms,
    - Docker: build using debian:buster, then copy binaries into redis:latest.
    
    * CircleCI: refactoring
    
    * CircleCI: refactoring #2
    
    * CircleCI: github ssh keys for git lfs
    
    * CircleCI: revert to old image
    
    * CircleCI: disabled cache restore
    
    * CircleCI: added sudo
    
    * CircleCI: fixed git lfs handling
    
    * CircleCI: try redis:5.0.5-buster image once again
    
    * CircleCI: so maybe python:3.7.4-buster
    
    * Moved Paella to Python 3, bumped version to 0.3.2
    
    * Added getpy to readies/paella
    
    * Build fix #1
    
    * Build fix #2
    
    * Dockerfile sh syntax, DEBUG flag in Makefile
    
    * system-setup: added popen.communicate()
    
    * Build: better cpu/gpu separation
    
    * Docker fixes
    
    * Build fix #3
    
    * Build fix #4
    
    * Fixes from review, updated README
    
    * Fixes from review #2
    
    - Restored README.md
    - Moved Makefile and system-setup.py to automation/
    - Removed setup and show-setup from Makefile
    - Removed LD_LIBRARY_PATH from Makefile
    - Moved deps/readies into automation/
    - Will provice automation/README.md in a subsequent PR
    - Added default value to DEVICE in CMakeLists.txt
    
    * Fixes from review #3
    
    * Fixes from review #4
    
    * Fixes from review #5
    
    * Fixes from review #6
    
    * Fixes from review #7
    
    * CircleCI cache hacking
    
    * CircleCI cache: cancelled fallback

--- get_deps.sh
+++ get_deps.sh
@@ -1,142 +1,185 @@
 #!/usr/bin/env bash
 
+HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
 set -e
-set -x
+[[ $VERBOSE == 1 ]] && set -x
 
-BASE_DIRECTORY=`pwd`
-
-# Allow different deps for different platforms:
-if [ -z "$DEPS_DIRECTORY" ]; then
-  DEPS_DIRECTORY=${BASE_DIRECTORY}/deps
+if [[ "$1" == "cpu" ]]; then
+	GPU=no
+	DEVICE=cpu
+elif [[ "$1" == "gpu" ]]; then
+	GPU=yes
+	DEVICE=gpu
+else
+	GPU=${GPU:-no}
+	if [[ $GPU == 1 ]]; then
+		DEVICE=gpu
+	else
+		DEVICE=cpu
+	fi
 fi
 
-mkdir -p ${DEPS_DIRECTORY}
-cd ${DEPS_DIRECTORY}
-DEPS_DIRECTORY=$PWD # -- to avoid relative/absolute confusion
+DEPS_DIR=$HERE/deps
+mkdir -p ${DEPS_DIR}
+cd ${DEPS_DIR}
 
-PREFIX=${DEPS_DIRECTORY}/install
+PREFIX=${DEPS_DIR}/install
 mkdir -p ${PREFIX}
 
 DLPACK_PREFIX=${PREFIX}/dlpack
-rm -rf ${DLPACK_PREFIX}
-mkdir -p ${DLPACK_PREFIX}
-
 TF_PREFIX=${PREFIX}/libtensorflow
-rm -rf ${TF_PREFIX}
-mkdir -p ${TF_PREFIX}
-
 TORCH_PREFIX=${PREFIX}/libtorch
-rm -rf ${TORCH_PREFIX}
-mkdir -p ${TORCH_PREFIX}
-
 ORT_PREFIX=${PREFIX}/onnxruntime
-rm -rf ${ORT_PREFIX}
-mkdir -p ${ORT_PREFIX}
 
-if [ ! -d dlpack ]; then
-    echo "Cloning dlpack"
+## DLPACK
+
+[[ $FORCE == 1 ]] && rm -rf ${DLPACK_PREFIX}
+
+if [[ ! -d dlpack ]]; then
+    echo "Cloning dlpack ..."
     git clone --depth 1 https://github.com/dmlc/dlpack.git
+	echo "Done."
+else
+  echo "dlpack is in place."
 fi
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  cp -d -r --no-preserve=ownership dlpack/include ${DLPACK_PREFIX}
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  cp -r dlpack/include ${DLPACK_PREFIX}
+if [[ ! -d ${DLPACK_PREFIX}/include ]]; then
+	mkdir -p ${DLPACK_PREFIX}
+	ln -sf ${DEPS_DIR}/dlpack/include ${DLPACK_PREFIX}/include
 fi
 
 ## TENSORFLOW
+
 TF_VERSION="1.12.0"
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  TF_OS="linux"
-  if [[ "$1" == "cpu" ]]; then
+
+[[ $FORCE == 1 ]] && rm -rf ${TF_PREFIX}
+
+if [[ ! -d ${TF_PREFIX} ]]; then
+  echo "Installing TensorFlow ..."
+
+  mkdir -p ${TF_PREFIX}
+
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    TF_OS="linux"
+    if [[ "$1" == "cpu" ]]; then
+      TF_BUILD="cpu"
+    else
+      TF_BUILD="gpu"
+    fi
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    TF_OS="darwin"
     TF_BUILD="cpu"
-  else
-    TF_BUILD="gpu"
   fi
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  TF_OS="darwin"
-  TF_BUILD="cpu"
+  
+  LIBTF_ARCHIVE=libtensorflow-${TF_BUILD}-${TF_OS}-x86_64-${TF_VERSION}.tar.gz
+  
+  if [ ! -e ${LIBTF_ARCHIVE} ]; then
+    echo "Downloading libtensorflow ${TF_VERSION} ${TF_BUILD}"
+    wget -q https://storage.googleapis.com/tensorflow/libtensorflow/${LIBTF_ARCHIVE}
+  fi
+  
+  tar xf ${LIBTF_ARCHIVE} --no-same-owner --strip-components=1 -C ${TF_PREFIX}
+  
+  echo "Done."
+else
+  echo "TensorFlow is in place."
 fi
-
-LIBTF_ARCHIVE=libtensorflow-${TF_BUILD}-${TF_OS}-x86_64-${TF_VERSION}.tar.gz
-
-if [ ! -e ${LIBTF_ARCHIVE} ]; then
-  echo "Downloading libtensorflow ${TF_VERSION} ${TF_BUILD}"
-  wget https://storage.googleapis.com/tensorflow/libtensorflow/${LIBTF_ARCHIVE}
-fi
-
-tar xf ${LIBTF_ARCHIVE} --no-same-owner --strip-components=1 -C ${TF_PREFIX}
 
 ## PYTORCH
 
 PT_VERSION="1.1.0"
 #PT_VERSION="latest"
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  PT_OS="shared-with-deps"
-  if [[ "$1" == "cpu" ]]; then
+[[ $FORCE == 1 ]] && rm -rf ${TORCH_PREFIX}
+
+if [[ ! -d ${TORCH_PREFIX} ]]; then
+  echo "Installing libtorch ..."
+
+  mkdir -p ${TORCH_PREFIX}
+
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    PT_OS="shared-with-deps"
+    if [[ "$1" == "cpu" ]]; then
+      PT_BUILD="cpu"
+    else
+      PT_BUILD="cu90"
+    fi
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    PT_OS="macos"
     PT_BUILD="cpu"
-  else
-    PT_BUILD="cu90"
   fi
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  PT_OS="macos"
-  PT_BUILD="cpu"
-fi
-
-if [[ "$PT_VERSION" == "latest" ]]; then
-  PT_BUILD=nightly/${PT_BUILD}
-fi
-
-# Where to get the archive
-LIBTORCH_URL=https://download.pytorch.org/libtorch/${PT_BUILD}/libtorch-${PT_OS}-${PT_VERSION}.zip
-# Directory where torch is extracted to
-LIBTORCH_DIRECTORY=libtorch-${PT_OS}-${PT_VERSION}
-
-# Archive - specifically named
-LIBTORCH_ARCHIVE=libtorch-${PT_OS}-${PT_BUILD}-${PT_VERSION}.zip
-
-if [ ! -e "${LIBTORCH_ARCHIVE}" ]; then
-  echo "Downloading libtorch ${PT_VERSION} ${PT_BUILD}"
-  curl -L ${LIBTORCH_URL} > ${LIBTORCH_ARCHIVE}
-fi
-
-unzip -o ${LIBTORCH_ARCHIVE}
-tar cf - libtorch | tar xf -  --no-same-owner --strip-components=1 -C ${TORCH_PREFIX}
-rm -rf libtorch
-
-if [[ "${PT_OS}" == "macos" ]]; then
-  # also download mkl
-  MKL_BUNDLE=mklml_mac_2019.0.3.20190220
-  if [ ! -e "${MKL_BUNDLE}.tgz" ]; then
-    wget "https://github.com/intel/mkl-dnn/releases/download/v0.18/${MKL_BUNDLE}.tgz"
+  
+  if [[ "$PT_VERSION" == "latest" ]]; then
+    PT_BUILD=nightly/${PT_BUILD}
   fi
-  tar xf ${MKL_BUNDLE}.tgz --no-same-owner --strip-components=1 -C ${TORCH_PREFIX}
-  tar xf ${MKL_BUNDLE}.tgz --no-same-owner --strip-components=1 -C ${ORT_PREFIX}
+  
+  # Where to get the archive
+  LIBTORCH_URL=https://download.pytorch.org/libtorch/${PT_BUILD}/libtorch-${PT_OS}-${PT_VERSION}.zip
+  # Directory where torch is extracted to
+  LIBTORCH_DIRECTORY=libtorch-${PT_OS}-${PT_VERSION}
+  
+  # Archive - specifically named
+  LIBTORCH_ARCHIVE=libtorch-${PT_OS}-${PT_BUILD}-${PT_VERSION}.zip
+  
+  if [ ! -e "${LIBTORCH_ARCHIVE}" ]; then
+    echo "Downloading libtorch ${PT_VERSION} ${PT_BUILD}"
+    curl -s -L ${LIBTORCH_URL} > ${LIBTORCH_ARCHIVE}
+  fi
+  
+  unzip -q -o ${LIBTORCH_ARCHIVE} -d ${TORCH_PREFIX}/../
+  
+  echo "Done."
+  
+  if [[ "${PT_OS}" == "macos" ]]; then
+	echo "Installing MKL ..."
+    # also download mkl
+    MKL_BUNDLE=mklml_mac_2019.0.3.20190220
+    if [ ! -e "${MKL_BUNDLE}.tgz" ]; then
+      wget -q "https://github.com/intel/mkl-dnn/releases/download/v0.18/${MKL_BUNDLE}.tgz"
+    fi
+    tar xzf ${MKL_BUNDLE}.tgz --no-same-owner --strip-components=1 -C ${TORCH_PREFIX}
+	mkdir -p ${ORT_PREFIX}
+    tar xzf ${MKL_BUNDLE}.tgz --no-same-owner --strip-components=1 -C ${ORT_PREFIX}
+	echo "Done."
+  fi
+else
+  echo "librotch is in place."
 fi
 
 ## ONNXRUNTIME
 ORT_VERSION="0.5.0"
+ORT_ARCH="x64"
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   if [[ "$1" == "cpu" ]]; then
-    ORT_OS="linux-x64"
-    ORT_BUILD="cpu"
+    ORT_OS="linux"
+    ORT_BUILD=""
   else
-    ORT_OS="linux-x64-gpu"
-    ORT_BUILD="gpu"
+    ORT_OS="linux"
+    ORT_BUILD="-gpu"
   fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  ORT_OS="osx-x64"
+  ORT_OS="osx"
   ORT_BUILD=""
 fi
 
-ORT_ARCHIVE=onnxruntime-${ORT_OS}-${ORT_VERSION}.tgz
+[[ $FORCE == 1 ]] && rm -rf ${ORT_PREFIX}
 
-if [ ! -e ${ORT_ARCHIVE} ]; then
-  echo "Downloading ONNXRuntime ${ORT_VERSION} ${ORT_BUILD}"
-  wget https://github.com/Microsoft/onnxruntime/releases/download/v${ORT_VERSION}/${ORT_ARCHIVE}
+ORT_ARCHIVE=onnxruntime-${ORT_OS}-${ORT_ARCH}${ORT_BUILD}-${ORT_VERSION}.tgz
+
+if [[ ! -d ${ORT_PREFIX} ]]; then
+  echo "Installing ONNXRuntime ..."
+
+  mkdir -p ${ORT_PREFIX}
+
+  if [ ! -e ${ORT_ARCHIVE} ]; then
+    echo "Downloading ONNXRuntime ${ORT_VERSION} ${DEVICE}"
+    wget -q https://github.com/microsoft/onnxruntime/releases/download/v${ORT_VERSION}/${ORT_ARCHIVE}
+  fi
+  
+  tar xf ${ORT_ARCHIVE} --no-same-owner --strip-components=1 -C ${ORT_PREFIX}
+else
+  echo "onnxruntime is in place."
 fi
 
-tar xf ${ORT_ARCHIVE} --no-same-owner --strip-components=1 -C ${ORT_PREFIX}
-
-echo "Done"
+echo "Done."
