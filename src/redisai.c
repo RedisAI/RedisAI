744dbdc33f60740f7bbe70a28ca2dff05d889e04
filipecosta90 <filipecosta.90@gmail.com>
2019-09-19 09:08:25 +0100

    [fix] run_queue_info->run_queue_mutex mutex was not initialized. [add] adding run queue condition var (future work). [fix] change from khash to array of queues

--- src/redisai.c
+++ src/redisai.c
@@ -12,7 +12,6 @@
 #include "rmutil/alloc.h"
 #include "util/arr_rm_alloc.h"
 #include "rmutil/args.h"
-#include "rmutil/khash.h"
 
 #define REDISAI_H_INCLUDE
 #include "redisai.h"
@@ -89,6 +88,7 @@ void queueRelease(queue *queue) {
 
 typedef struct RunQueueInfo {
   pthread_mutex_t run_queue_mutex;
+  pthread_cond_t queue_condition_var;
   queue *run_queue;
   pthread_t run_thread_id;
 } RunQueueInfo;
@@ -101,11 +101,10 @@ void freeRunQueueInfo(RunQueueInfo* info) {
   RedisModule_Free(info);
 }
 
-KHASH_MAP_INIT_INT64(run_queues, RunQueueInfo *);
-
-static kh_run_queues_t* run_queues = NULL;
+static RunQueueInfo **run_queues = NULL;
 
 static pthread_mutex_t global_run_queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t  global_run_queue_condition_var   = PTHREAD_COND_INITIALIZER;
 
 void *RedisAI_Run_ThreadMain(void *arg);
 
@@ -114,41 +113,37 @@ size_t deviceToKey(RAI_Device device, int64_t deviceid) {
   return device * (RAI_MAX_DEVICE_ID + 1) + (deviceid + 1);
 }
 
+/* Ensure that the the run queue for the device exists.
+ * If not, create it. As an example, if you ask for queue number 4,
+ * all queues for devices behind it ( 0,1,2,3 ) will be created if they don't exist. */
 int ensureRunQueue(RAI_Device device, int64_t deviceid) {
+
   if (deviceid < -1 || deviceid > RAI_MAX_DEVICE_ID) {
-    return 0;
-  }
-
-  size_t key = deviceToKey(device, deviceid);
-
-  khiter_t key_iter = kh_get_run_queues(run_queues, key);
-  if (key_iter != kh_end(run_queues)) {
-    return 1;
-  }
-
-  RunQueueInfo* run_queue_info = RedisModule_Calloc(1, sizeof(RunQueueInfo));
-  // run_queue->run_queue_mutex = PTHREAD_MUTEX_INITIALIZER;
-  run_queue_info->run_queue = queueCreate();
-
-  pthread_t tid;
-
-  if (pthread_create(&tid, NULL, RedisAI_Run_ThreadMain, run_queue_info) != 0) {
-    freeRunQueueInfo(run_queue_info);
     return REDISMODULE_ERR;
   }
 
-  run_queue_info->run_thread_id = tid;
+  const size_t queue_number = deviceToKey(device, deviceid);
 
-  int dummy;
+  if (queue_number<0) {
+        return REDISMODULE_ERR;
+  }
 
-  pthread_mutex_lock(&global_run_queue_mutex);
+  while (queue_number >= array_len(run_queues)){
+    RunQueueInfo *run_queue_info = RedisModule_Alloc(sizeof(RunQueueInfo));
+    run_queue_info->run_queue = queueCreate();
+    pthread_cond_init(&run_queue_info->queue_condition_var, NULL);
+    pthread_mutex_init(&run_queue_info->run_queue_mutex, NULL);
+    pthread_t tid;
 
-  khiter_t iter = kh_put_run_queues(run_queues, key, &dummy);
-  kh_value(run_queues, iter) = run_queue_info;
+    if (pthread_create(&tid, NULL, RedisAI_Run_ThreadMain, run_queue_info) != 0){
+      freeRunQueueInfo(run_queue_info);
+      return REDISMODULE_ERR;
+    }
+    run_queue_info->run_thread_id = tid;
+    run_queues = array_append(run_queues, run_queue_info);
+  }
 
-  pthread_mutex_unlock(&global_run_queue_mutex);
-
-  return 1;
+  return REDISMODULE_OK;
 }
 
 static pthread_mutex_t runQueueMutex = PTHREAD_MUTEX_INITIALIZER;
@@ -600,7 +595,7 @@ int RedisAI_ModelSet_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv,
 
   // TODO: if backend loaded, make sure there's a queue
 
-  if (!ensureRunQueue(device, deviceid)) {
+  if (ensureRunQueue(device, deviceid)==REDISMODULE_ERR) {
     RAI_ModelFree(model, &err);
     if (err.code != RAI_OK) {
       #ifdef RAI_PRINT_BACKEND_ERRORS
@@ -1006,25 +1001,22 @@ int RedisAI_ModelRun_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv,
   //  RedisModule_AbortBlock(bc);
   //  return RedisModule_ReplyWithError(ctx, "-ERR Can't start thread");
 
- // TODO: evaluate if kh is thread-safe when reading
 
   size_t queue_key = deviceToKey(mto->device, mto->deviceid);
-
   pthread_mutex_lock(&global_run_queue_mutex);
-  khiter_t key_iter = kh_get_run_queues(run_queues, queue_key);
-  if (key_iter == kh_end(run_queues)) {
-    pthread_mutex_unlock(&global_run_queue_mutex);
-    return RedisModule_ReplyWithError(ctx, "Queue not initialized for device.");
+  if(queue_key >= array_len(run_queues)){
+      pthread_mutex_unlock(&global_run_queue_mutex);
+      return RedisModule_ReplyWithError(ctx, "Queue not initialized for device.");
   }
-  RunQueueInfo run_queue_info = *kh_value(run_queues, key_iter);
+  RunQueueInfo* run_queue_info = run_queues[queue_key];
   pthread_mutex_unlock(&global_run_queue_mutex);
 
   rinfo->client = RedisModule_BlockClient(ctx, RedisAI_Run_Reply, NULL, RedisAI_FreeData, 0);
   // RedisModule_SetDisconnectCallback(rinfo->client, RedisAI_Disconnected);
 
-  pthread_mutex_lock(&run_queue_info.run_queue_mutex);
-  queuePush(run_queue_info.run_queue, rinfo);
-  pthread_mutex_unlock(&run_queue_info.run_queue_mutex);
+  pthread_mutex_lock(&run_queue_info->run_queue_mutex);
+  queuePush(run_queue_info->run_queue, rinfo);
+  pthread_mutex_unlock(&run_queue_info->run_queue_mutex);
 
   // RedisAI_RunSession(rinfo);
   // RedisAI_FreeRunInfo(ctx, rinfo);
@@ -1039,19 +1031,19 @@ void *RedisAI_Run_ThreadMain(void *arg) {
   RunQueueInfo* run_queue_info = (RunQueueInfo*)arg;
 
   while(1) {
-    pthread_mutex_lock(&run_queue_info->run_queue_mutex);
-    queueItem *item = queuePop(run_queue_info->run_queue);
-    pthread_mutex_unlock(&run_queue_info->run_queue_mutex);
+  pthread_mutex_lock(&run_queue_info->run_queue_mutex);
+  queueItem *item = queuePop(run_queue_info->run_queue);
+  pthread_mutex_unlock(&run_queue_info->run_queue_mutex);
 
-    if (item) {
-      RedisAI_RunSession(item->value);
-      RedisModule_Free(item);
-      
-    } 
-    else {
-      // only sleep if the last Pop was empty
-      usleep(1000);
-    }
+  if (item) {
+    RedisAI_RunSession(item->value);
+    RedisModule_Free(item);
+    
+  } 
+  else {
+    // only sleep if the last Pop was empty
+    usleep(1000);
+  }
     // release item (note: the callback does it; verify)
     // unblock (the callback does it)
   }
@@ -1167,23 +1159,21 @@ int RedisAI_ScriptRun_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv
   rinfo->outkeys = outkeys;
   rinfo->err = NULL;
 
-  size_t queue_key = deviceToKey(sto->device, sto->deviceid);
-
-  pthread_mutex_lock(&global_run_queue_mutex);
-  khiter_t key_iter = kh_get_run_queues(run_queues, queue_key);
-  if (key_iter == kh_end(run_queues)) {
+    size_t queue_key = deviceToKey(sto->device, sto->deviceid);
+    pthread_mutex_lock(&global_run_queue_mutex);
+    if(queue_key >= array_len(run_queues)){
+        pthread_mutex_unlock(&global_run_queue_mutex);
+        return RedisModule_ReplyWithError(ctx, "Queue not initialized for device.");
+    }
+    RunQueueInfo* run_queue_info = run_queues[queue_key];
     pthread_mutex_unlock(&global_run_queue_mutex);
-    return RedisModule_ReplyWithError(ctx, "Queue not initialized for device.");
-  }
-  RunQueueInfo run_queue_info = *kh_value(run_queues, key_iter);
-  pthread_mutex_unlock(&global_run_queue_mutex);
 
   rinfo->client = RedisModule_BlockClient(ctx, RedisAI_Run_Reply, NULL, RedisAI_FreeData, 0);
   // RedisModule_SetDisconnectCallback(rinfo->client, RedisAI_Disconnected);
 
-  pthread_mutex_lock(&run_queue_info.run_queue_mutex);
-  queuePush(run_queue_info.run_queue, rinfo);
-  pthread_mutex_unlock(&run_queue_info.run_queue_mutex);
+  pthread_mutex_lock(&run_queue_info->run_queue_mutex);
+  queuePush(run_queue_info->run_queue, rinfo);
+  pthread_mutex_unlock(&run_queue_info->run_queue_mutex);
 
   RedisModule_ReplicateVerbatim(ctx);
  
@@ -1341,7 +1331,7 @@ int RedisAI_ScriptSet_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv
     return ret;
   }
 
-  if (!ensureRunQueue(device, deviceid)) {
+  if (ensureRunQueue(device, deviceid)==REDISMODULE_ERR) {
     RAI_ScriptFree(script, &err);
     if (err.code != RAI_OK) {
       #ifdef RAI_PRINT_BACKEND_ERRORS
@@ -1631,11 +1621,15 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
     }
   }
 
-  run_queues = kh_init_run_queues();
+  run_queues = array_new(RunQueueInfo, 1);
+  size_t key = deviceToKey(RAI_DEVICE_CPU, -1);
 
-  // if (!ensureRunQueue(RAI_DEVICE_CPU, -1)) {
-  //   return REDISMODULE_ERR;
-  // }
+  if (ensureRunQueue(RAI_DEVICE_CPU, -1) != REDISMODULE_OK){
+    RedisModuleString *message = RedisModule_CreateStringPrintf(ctx, "Queue not initialized for device  %d", key);
+    RedisModule_Log(ctx, "warning", RedisModule_StringPtrLen(message, NULL));
+    return REDISMODULE_ERR;
+  }
+  RedisModule_Log(ctx, "info", RedisModule_StringPtrLen(RedisModule_CreateStringPrintf(ctx, "Total number of initialized queues %d", (array_len(run_queues))), NULL));
 
   return REDISMODULE_OK;
 }
