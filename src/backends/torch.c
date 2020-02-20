7d6dc4793b941c8e269d75d986c5ba4b7e75a0a0
Luca Antiga <luca.antiga@orobix.com>
2020-02-20 10:16:34 +0100

    Fix memleaks from Valgrind runs [WIP]

--- src/backends/torch.c
+++ src/backends/torch.c
@@ -154,7 +154,6 @@ RAI_Script *RAI_ScriptCreateTorch(const char* devicestr, const char *scriptdef, 
   ret->devicestr = RedisModule_Strdup(devicestr);
   ret->refCount = 1;
   
-
   return ret;
 }
 
@@ -162,6 +161,7 @@ void RAI_ScriptFreeTorch(RAI_Script* script, RAI_Error* error) {
 
   torchDeallocContext(script->script);
   RedisModule_Free(script->scriptdef);
+  RedisModule_Free(script->devicestr);
   RedisModule_Free(script);
 }
 
