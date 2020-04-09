4ec5163e2812db0b5829d7d8f174ba93294e4492
filipe oliveira <filipecosta.90@gmail.com>
2020-04-09 08:56:02 +0100

    Performance improvement on AI.TENSORSET when setting data with BLOB format (#317)
    * [add] using RedisModule_RetainString on AI.TENSORSET key type dim1..dimN BLOB ...
    
    * [add] adding to CI the simple test with multiproc AI.TENSORSET ... BLOB ...
    
    * [add] extended testing for tensorset and modelrun
    
    * [fix] adding numpy version to make sure we have rng
    
    * [add] updated numpy test requirements to 1.17 (use rng )
    
    * [fix] proper naming for tensorflow modelrun added test
    
    * [fix] quick merge fix
    
    * Fix test
    
    Co-authored-by: Luca Antiga <luca.antiga@orobix.com>

--- test/tests_common.py
+++ test/tests_common.py
@@ -55,13 +55,13 @@ def test_common_tensorset_error_replies(env):
         env.assertEqual(type(exception), redis.exceptions.ResponseError)
         env.assertEqual(exception.__str__(), "invalid data type")
 
-    # ERR negative value found in tensor shape
+    # ERR invalid or negative value found in tensor shape
     try:
         con.execute_command('AI.TENSORSET', 'z', 'INT32', -1, 'VALUES', 2, 3)
     except Exception as e:
         exception = e
         env.assertEqual(type(exception), redis.exceptions.ResponseError)
-        env.assertEqual("negative value found in tensor shape",exception.__str__())
+        env.assertEqual("invalid or negative value found in tensor shape",exception.__str__())
 
     # ERR invalid argument found in tensor shape
     try:
@@ -69,7 +69,7 @@ def test_common_tensorset_error_replies(env):
     except Exception as e:
         exception = e
         env.assertEqual(type(exception), redis.exceptions.ResponseError)
-        env.assertEqual("invalid argument found in tensor shape",exception.__str__())
+        env.assertEqual("invalid or negative value found in tensor shape",exception.__str__())
 
     # ERR invalid value
     try:
--- test/tests_pytorch.py
+++ test/tests_pytorch.py
@@ -18,6 +18,12 @@ def test_pytorch_modelrun(env):
     model_filename = os.path.join(test_data_path, 'pt-minimal.pt')
     wrong_model_filename = os.path.join(test_data_path, 'graph.pb')
 
+    ret = con.execute_command('AI.TENSORSET', 'a', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
+    env.assertEqual(ret, b'OK')
+
+    ret = con.execute_command('AI.TENSORSET', 'b', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
+    env.assertEqual(ret, b'OK')
+
     with open(model_filename, 'rb') as f:
         model_pb = f.read()
 
@@ -64,9 +70,6 @@ def test_pytorch_modelrun(env):
     except Exception as e:
         exception = e
         env.assertEqual(type(exception), redis.exceptions.ResponseError)
-
-    env.execute_command('AI.TENSORSET', 'a', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
-    env.execute_command('AI.TENSORSET', 'b', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
 
     try:
         con.execute_command('AI.MODELRUN', 'm_1', 'INPUTS', 'a', 'b', 'OUTPUTS')
@@ -191,10 +194,10 @@ def test_pytorch_modelinfo(env):
     ret = con.execute_command('AI.MODELSET', 'm', 'TORCH', DEVICE, 'TAG', 'asdf', model_pb)
     env.assertEqual(ret, b'OK')
 
-    ret = env.execute_command('AI.TENSORSET', 'a', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
+    ret = con.execute_command('AI.TENSORSET', 'a', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
     env.assertEqual(ret, b'OK')
 
-    ret = env.execute_command('AI.TENSORSET', 'b', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
+    ret = con.execute_command('AI.TENSORSET', 'b', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
     env.assertEqual(ret, b'OK')
 
     ensureSlaveSynced(con, env)
@@ -575,10 +578,10 @@ def test_pytorch_modelrun_disconnect(env):
     ret = con.execute_command('AI.MODELSET', 'm', 'TORCH', DEVICE, model_pb)
     env.assertEqual(ret, b'OK')
 
-    ret = env.execute_command('AI.TENSORSET', 'a', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
+    ret = con.execute_command('AI.TENSORSET', 'a', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
     env.assertEqual(ret, b'OK')
 
-    ret = env.execute_command('AI.TENSORSET', 'b', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
+    ret = con.execute_command('AI.TENSORSET', 'b', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
     env.assertEqual(ret, b'OK')
 
     ensureSlaveSynced(con, env)
@@ -652,8 +655,8 @@ def test_pytorch_model_rdb_save_load(env):
 
     model_serialized_memory = con.execute_command('AI.MODELGET', 'm', 'BLOB')
 
-    env.execute_command('AI.TENSORSET', 'a', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
-    env.execute_command('AI.TENSORSET', 'b', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
+    con.execute_command('AI.TENSORSET', 'a', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
+    con.execute_command('AI.TENSORSET', 'b', 'FLOAT', 2, 2, 'VALUES', 2, 3, 2, 3)
     con.execute_command('AI.MODELRUN', 'm', 'INPUTS', 'a', 'b', 'OUTPUTS', 'c')
     dtype_memory, shape_memory, data_memory = con.execute_command('AI.TENSORGET', 'c', 'VALUES')
 
