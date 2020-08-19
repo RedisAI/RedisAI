ae6da4b33ad0f44dea96bb21ba9b66ce074a1675
filipecosta90 <filipecosta.90@gmail.com>
2020-08-19 10:20:07 +0100

    [fix] Fixes per PR review: identation

--- test/unit/unit_tests_err.cpp
+++ test/unit/unit_tests_err.cpp
@@ -1,64 +1,61 @@
-
 #include <limits.h>
 #include "gtest/gtest.h"
-
 
 extern "C" {
 #include "src/redismodule.h"
 #include "src/err.h"
 }
 
-
 namespace {
 
-    void Alloc_Reset() {
-        RedisModule_Alloc = malloc;
-        RedisModule_Realloc = realloc;
-        RedisModule_Calloc = calloc;
-        RedisModule_Free = free;
-        RedisModule_Strdup = strdup;
-    }
+void Alloc_Reset() {
+    RedisModule_Alloc = malloc;
+    RedisModule_Realloc = realloc;
+    RedisModule_Calloc = calloc;
+    RedisModule_Free = free;
+    RedisModule_Strdup = strdup;
+}
 
-    TEST(err, RAI_GetError) {
-        Alloc_Reset();
-        RAI_Error* err;
-        EXPECT_EQ(0,RAI_InitError(&err));
-        EXPECT_STREQ(RAI_GetError(err),nullptr);
-        EXPECT_STREQ(RAI_GetErrorOneLine(err),nullptr);
-        EXPECT_EQ(RAI_GetErrorCode(err),RAI_OK);
-        RAI_FreeError(err);
-    }
+TEST(err, RAI_GetError) {
+    Alloc_Reset();
+    RAI_Error* err;
+    EXPECT_EQ(0,RAI_InitError(&err));
+    EXPECT_STREQ(RAI_GetError(err),nullptr);
+    EXPECT_STREQ(RAI_GetErrorOneLine(err),nullptr);
+    EXPECT_EQ(RAI_GetErrorCode(err),RAI_OK);
+    RAI_FreeError(err);
+}
 
-    TEST(err, RAI_SetError_nullptr) {
-        Alloc_Reset();
-        RAI_Error* err = nullptr;
+TEST(err, RAI_SetError_nullptr) {
+    Alloc_Reset();
+    RAI_Error* err = nullptr;
 
-        // Test for nullptr err
-        RAI_SetError(err,RAI_OK,nullptr);
+    // Test for nullptr err
+    RAI_SetError(err,RAI_OK,nullptr);
 
-        // ensure RAI_FreeError on nullptr is safe
-        RAI_FreeError(err);
+    // ensure RAI_FreeError on nullptr is safe
+    RAI_FreeError(err);
 
-        // ensure RAI_ClearError on nullptr is safe
-        RAI_ClearError(err);
-    }
+    // ensure RAI_ClearError on nullptr is safe
+    RAI_ClearError(err);
+}
 
-    TEST(err, RAI_SetError_default) {
-        Alloc_Reset();
-        RAI_Error* err;
-        EXPECT_EQ(0,RAI_InitError(&err));
-        RAI_SetError(err,RAI_OK,nullptr);
-        EXPECT_STREQ(RAI_GetErrorOneLine(err),"ERR Generic error");
-        EXPECT_EQ(RAI_GetErrorCode(err),RAI_OK);
+TEST(err, RAI_SetError_default) {
+    Alloc_Reset();
+    RAI_Error* err;
+    EXPECT_EQ(0,RAI_InitError(&err));
+    RAI_SetError(err,RAI_OK,nullptr);
+    EXPECT_STREQ(RAI_GetErrorOneLine(err),"ERR Generic error");
+    EXPECT_EQ(RAI_GetErrorCode(err),RAI_OK);
 
-        // clear error to reuse structure
-        RAI_ClearError(err);
+    // clear error to reuse structure
+    RAI_ClearError(err);
 
-        RAI_SetError(err,RAI_EMODELCREATE,"ERR specific error message");
-        EXPECT_STREQ(RAI_GetErrorOneLine(err),"ERR specific error message");
-        EXPECT_EQ(RAI_GetErrorCode(err),RAI_EMODELCREATE);
+    RAI_SetError(err,RAI_EMODELCREATE,"ERR specific error message");
+    EXPECT_STREQ(RAI_GetErrorOneLine(err),"ERR specific error message");
+    EXPECT_EQ(RAI_GetErrorCode(err),RAI_EMODELCREATE);
 
-        RAI_FreeError(err);
-    }
+    RAI_FreeError(err);
+}
 
 }  // namespace
