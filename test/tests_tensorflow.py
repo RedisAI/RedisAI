da632b2d0702f978ba2c74d4b9e83ae7c79b1f5e
Luca Antiga <luca.antiga@orobix.com>
2020-11-06 12:32:21 +0100

    Fix keys at pos after TIMEOUT

--- test/tests_tensorflow.py
+++ test/tests_tensorflow.py
@@ -689,6 +689,9 @@ def test_tensorflow_modelrun_with_timeout(env):
                         'BLOB', img.tobytes())
 
     con = env.getConnection()
+    print('AI.MODELRUN', model_name,
+                        'TIMEOUT', timeout,
+                        'INPUTS', 'input{1}', 'OUTPUTS', 'output{1}')
     con.execute_command('AI.MODELRUN', model_name,
                         'TIMEOUT', timeout,
                         'INPUTS', 'input{1}', 'OUTPUTS', 'output{1}')
