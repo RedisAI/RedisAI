aaf0dff1bc644de36c7b4aedbd5ee5c680522e7a
filipe oliveira <filipecosta.90@gmail.com>
2020-11-27 08:54:07 +0000

    Applied clang-format to src/* files. Failing CI if linter rules not followed (#514)

--- src/util/arr.h
+++ src/util/arr.h
@@ -32,18 +32,18 @@
  * to those of the RM_ family
  */
 #ifndef array_alloc_fn
-#define array_alloc_fn malloc
+#define array_alloc_fn   malloc
 #define array_realloc_fn realloc
-#define array_free_fn free
+#define array_free_fn    free
 #endif
 
 typedef struct {
-  uint32_t len;
-  // TODO: optimize memory by making cap a 16-bit delta from len, and elem_sz 16 bit as well. This
-  // makes the whole header fit in 64 bit
-  uint32_t cap;
-  uint32_t elem_sz;
-  char buf[];
+    uint32_t len;
+    // TODO: optimize memory by making cap a 16-bit delta from len, and elem_sz 16 bit as well. This
+    // makes the whole header fit in 64 bit
+    uint32_t cap;
+    uint32_t elem_sz;
+    char buf[];
 } array_hdr_t;
 
 typedef void *array_t;
@@ -57,11 +57,11 @@ typedef void *array_t;
 /* Initialize a new array with a given element size and capacity. Should not be used directly - use
  * array_new instead */
 static array_t array_new_sz(uint32_t elem_sz, uint32_t cap, uint32_t len) {
-  array_hdr_t *hdr = array_alloc_fn(sizeof(array_hdr_t) + cap * elem_sz);
-  hdr->cap = cap;
-  hdr->elem_sz = elem_sz;
-  hdr->len = len;
-  return (array_t)(hdr->buf);
+    array_hdr_t *hdr = array_alloc_fn(sizeof(array_hdr_t) + cap * elem_sz);
+    hdr->cap = cap;
+    hdr->elem_sz = elem_sz;
+    hdr->len = len;
+    return (array_t)(hdr->buf);
 }
 
 /* Initialize an array for a given type T with a given capacity and zero length. The array should be
@@ -79,47 +79,45 @@ static array_t array_new_sz(uint32_t elem_sz, uint32_t cap, uint32_t len) {
 #define array_newlen(T, len) (array_new_sz(sizeof(T), len, len))
 
 static inline array_t array_ensure_cap(array_t arr, uint32_t cap) {
-  array_hdr_t *hdr = array_hdr(arr);
-  if (cap > hdr->cap) {
-    hdr->cap = MAX(hdr->cap * 2, cap);
-    hdr = array_realloc_fn(hdr, array_sizeof(hdr));
-  }
-  return (array_t)hdr->buf;
+    array_hdr_t *hdr = array_hdr(arr);
+    if (cap > hdr->cap) {
+        hdr->cap = MAX(hdr->cap * 2, cap);
+        hdr = array_realloc_fn(hdr, array_sizeof(hdr));
+    }
+    return (array_t)hdr->buf;
 }
 
 /* Ensure capacity for the array to grow by one */
 static inline array_t array_grow(array_t arr) {
-  return array_ensure_cap(arr, ++array_hdr(arr)->len);
+    return array_ensure_cap(arr, ++array_hdr(arr)->len);
 }
 
 /* get the last element in the array */
 #define array_tail(arr) (arr[array_hdr(arr)->len - 1])
 
 /* Append an element to the array, returning the array which may have been reallocated */
-#define array_append(arr, x)   \
-  ({                           \
-    (arr) = array_grow((arr)); \
-    array_tail((arr)) = (x);   \
-    (arr);                     \
-  })
+#define array_append(arr, x)                                                                       \
+    ({                                                                                             \
+        (arr) = array_grow((arr));                                                                 \
+        array_tail((arr)) = (x);                                                                   \
+        (arr);                                                                                     \
+    })
 
 /* Get the length of the array */
-static inline uint32_t array_len(array_t arr) {
-  return arr ? array_hdr(arr)->len : 0;
-}
+static inline uint32_t array_len(array_t arr) { return arr ? array_hdr(arr)->len : 0; }
 
 static inline void *array_trimm(array_t arr, uint32_t len, uint32_t cap) {
-  array_hdr_t *arr_hdr = array_hdr(arr);
-  assert(len >= 0 && "trimming len is negative");
-  assert((cap == -1 || cap > 0 || len == cap) && "trimming capacity is illegal");
-  assert((cap == -1 || cap >= len) && "trimming len is greater then capacity");
-  assert((len <= arr_hdr->len) && "trimming len is greater then current len");
-  arr_hdr->len = len;
-  if (cap != -1) {
-    arr_hdr->cap = cap;
-    arr_hdr = array_realloc_fn(arr_hdr, array_sizeof(arr_hdr));
-  }
-  return arr_hdr->buf;
+    array_hdr_t *arr_hdr = array_hdr(arr);
+    assert(len >= 0 && "trimming len is negative");
+    assert((cap == -1 || cap > 0 || len == cap) && "trimming capacity is illegal");
+    assert((cap == -1 || cap >= len) && "trimming len is greater then capacity");
+    assert((len <= arr_hdr->len) && "trimming len is greater then current len");
+    arr_hdr->len = len;
+    if (cap != -1) {
+        arr_hdr->cap = cap;
+        arr_hdr = array_realloc_fn(arr_hdr, array_sizeof(arr_hdr));
+    }
+    return arr_hdr->buf;
 }
 
 #define array_trimm_len(arr, len) array_trimm(arr, len, -1)
@@ -128,9 +126,7 @@ static inline void *array_trimm(array_t arr, uint32_t len, uint32_t cap) {
 #define array_clear(arr) array_hdr(arr)->len = 0
 
 /* Free the array, without dealing with individual elements */
-static void array_free(array_t arr) {
-  array_free_fn(array_hdr(arr));
-}
+static void array_free(array_t arr) { array_free_fn(array_hdr(arr)); }
 
 /* Repeate the code in "blk" for each element in the array, and give it the name of "as".
  * e.g:
@@ -138,31 +134,31 @@ static void array_free(array_t arr) {
  *  arr = array_append(arr, 1);
  *  array_foreach(arr, i, printf("%d\n", i));
  */
-#define array_foreach(arr, as, blk)                 \
-  ({                                                \
-    for (uint32_t i = 0; i < array_len(arr); i++) { \
-      typeof(*arr) as = arr[i];                     \
-      blk;                                          \
-    }                                               \
-  })
+#define array_foreach(arr, as, blk)                                                                \
+    ({                                                                                             \
+        for (uint32_t i = 0; i < array_len(arr); i++) {                                            \
+            typeof(*arr) as = arr[i];                                                              \
+            blk;                                                                                   \
+        }                                                                                          \
+    })
 
 /* Free the array, freeing individual elements with free_cb */
-#define array_free_ex(arr, blk)                       \
-  ({                                                  \
-    if (arr) {                                        \
-      for (uint32_t i = 0; i < array_len(arr); i++) { \
-        void *ptr = &arr[i];                          \
-        { blk; }                                      \
-      }                                               \
-      array_free(arr);                                \
-    }                                                 \
-  })
+#define array_free_ex(arr, blk)                                                                    \
+    ({                                                                                             \
+        if (arr) {                                                                                 \
+            for (uint32_t i = 0; i < array_len(arr); i++) {                                        \
+                void *ptr = &arr[i];                                                               \
+                { blk; }                                                                           \
+            }                                                                                      \
+            array_free(arr);                                                                       \
+        }                                                                                          \
+    })
 
 /* Pop the top element from the array, reduce the size and return it */
-#define array_pop(arr)               \
-  ({                                 \
-    assert(array_hdr(arr)->len > 0); \
-    arr[--(array_hdr(arr)->len)];    \
-  })
+#define array_pop(arr)                                                                             \
+    ({                                                                                             \
+        assert(array_hdr(arr)->len > 0);                                                           \
+        arr[--(array_hdr(arr)->len)];                                                              \
+    })
 
 #endif
