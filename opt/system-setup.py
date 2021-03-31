305bf3b88c4d7a01069edca112a37132826990e6
rafie <rafi@redislabs.com>
2021-03-31 15:49:57 +0300

    fixes 2

--- opt/system-setup.py
+++ opt/system-setup.py
@@ -6,7 +6,6 @@ import argparse
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 ROOT = os.path.abspath(os.path.join(HERE, ".."))
-sys.path.insert(0, os.path.join(HERE, "readies"))
 READIES = os.path.join(ROOT, "opt/readies")
 sys.path.insert(0, READIES)
 import paella
@@ -44,11 +43,8 @@ class RedisAISetup(paella.Setup):
 
         self.run("%s/bin/getgcc --modern" % READIES)
 
-        self.run("""
-            dir=$(mktemp -d /tmp/tar.XXXXXX)
-            (cd $dir; wget -q -O tar.tgz http://redismodules.s3.amazonaws.com/gnu/gnu-tar-1.32-x64-centos7.tgz; tar -xzf tar.tgz -C /; )
-            rm -rf $dir
-            """)
+        if self.arch == 'x64':
+            self.install_linux_gnu_tar()
 
         if not self.dist == "amzn":
             self.install("epel-release")
@@ -64,23 +60,23 @@ class RedisAISetup(paella.Setup):
         self.install("python3-networkx")
         self.install_git_lfs_on_linux()
 
-    def macosx(self):
-        if sh('xcode-select -p') == '':
-            fatal("Xcode tools are not installed. Please run xcode-select --install.")
+    def linux_last(self):
+        self.install("valgrind")
 
+    def macos(self):
         self.install_gnu_utils()
-        self.install("cmake")
         self.install("git-lfs")
         self.install("redis")
 
     def common_last(self):
-        self.run("python3 -m pip uninstall -y ramp-packer RLTest || true")
-        # redis-py-cluster should be installed from git due to redis-py dependency
-        self.pip_install("--no-cache-dir git+https://github.com/Grokzen/redis-py-cluster.git@master")
-        self.pip_install("--no-cache-dir git+https://github.com/RedisLabsModules/RLTest.git@master")
-        self.pip_install("--no-cache-dir git+https://github.com/RedisLabs/RAMP@master")
+        self.run("%s/bin/getclang --format" % READIES)
+        if self.platform == "arm":
+            self.run("%s/bin/getcmake" % READIES)
+        else:
+            self.run("%s/bin/getcmake --no-repo" % READIES)
 
-        self.pip_install("-r %s/readies/paella/requirements.txt" % HERE)
+        self.run("{PYTHON} {READIES}/bin/getrmpytools".format(PYTHON=self.python, READIES=READIES))
+
         self.pip_install("-r %s/test/test_requirements.txt" % ROOT)
 
         self.pip_install("awscli")
