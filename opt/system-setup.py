349bc2fce526b3d6cc97153cd4404a9203bae020
Rafi Einstein <raffapen@outlook.com>
2021-04-18 13:19:36 +0300

    [1.0] CircleCI: added QA automation (#660) (#666)
    * [1.0] CircleCI: added QA automation (#660)
    * Updated readies
    * Disabled nightly build
    
    Co-authored-by: alonre24 <alonreshef24@gmail.com>

--- opt/system-setup.py
+++ opt/system-setup.py
@@ -6,7 +6,8 @@ import argparse
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 ROOT = os.path.abspath(os.path.join(HERE, ".."))
-sys.path.insert(0, os.path.join(HERE, "readies"))
+READIES = os.path.join(ROOT, "opt/readies")
+sys.path.insert(0, READIES)
 import paella
 
 #----------------------------------------------------------------------------------------------
@@ -17,79 +18,65 @@ class RedisAISetup(paella.Setup):
 
     def common_first(self):
         self.install_downloaders()
-        self.setup_pip()
-        self.pip_install("wheel virtualenv")
-        # if self.osnick == 'xenial': 
-        #     self.pip_install("setuptools --upgrade")
-        #     self.pip_install("-IU --force-reinstall setuptools")
+        self.pip_install("wheel")
 
-        if self.os == 'linux':
-            self.install("ca-certificates")
-        self.install("git unzip wget patchelf")
-        self.install("coreutils") # for realpath
+        self.install("git unzip patchelf")
+        if self.osnick != 'centos8':
+            self.install("coreutils") # for realpath
 
     def debian_compat(self):
+        self.run("%s/bin/enable-utf8" % READIES)
+        self.run("%s/bin/getgcc" % READIES)
         self.install("gawk")
-        self.install("build-essential cmake")
+        self.install("libssl-dev")
         self.install("python3-regex")
-        self.install("python3-psutil python3-networkx python3-numpy") # python3-skimage
+        self.install("python3-networkx python3-numpy")
+        if self.platform.is_arm():
+            self.install("python3-dev") # python3-skimage
+        self.install("libmpich-dev libopenblas-dev") # for libtorch
         self.install_git_lfs_on_linux()
 
     def redhat_compat(self):
+        self.run("%s/bin/enable-utf8" % READIES)
+        self.run("%s/bin/getepel" % READIES)
         self.install("redhat-lsb-core")
-        self.run("%s/readies/bin/enable-utf8" % HERE)
 
-        self.group_install("'Development Tools'")
-        self.install("cmake3")
-        self.run("ln -sf `command -v cmake3` /usr/local/bin/cmake")
+        self.run("%s/bin/getgcc --modern" % READIES)
 
-        self.install("centos-release-scl")
-        self.install("devtoolset-8")
-        self.run("cp /opt/rh/devtoolset-8/enable /etc/profile.d/scl-devtoolset-8.sh")
-        paella.mkdir_p("%s/profile.d" % ROOT)
-        self.run("cp /opt/rh/devtoolset-8/enable %s/profile.d/scl-devtoolset-8.sh" % ROOT)
-
-        self.run("""
-            dir=$(mktemp -d /tmp/tar.XXXXXX)
-            (cd $dir; wget -q -O tar.tgz http://redismodules.s3.amazonaws.com/gnu/gnu-tar-1.32-x64-centos7.tgz; tar -xzf tar.tgz -C /; )
-            rm -rf $dir
-            """)
+        if self.arch == 'x64':
+            self.install_linux_gnu_tar()
 
         if not self.dist == "amzn":
             self.install("epel-release")
             self.install("python3-devel libaec-devel")
-            self.install("python36-psutil")
         else:
             self.run("amazon-linux-extras install epel", output_on_error=True)
             self.install("python3-devel")
-            self.pip_install("psutil")
 
         self.install_git_lfs_on_linux()
 
     def fedora(self):
-        self.group_install("'Development Tools'")
-        self.install("cmake")
-        self.run("ln -sf `command -v cmake3` /usr/local/bin/cmake")
-        self.install("python3 python3-psutil python3-networkx")
+        self.run("%s/bin/getepel" % READIES)
+        self.install("python3-networkx")
         self.install_git_lfs_on_linux()
 
-    def macosx(self):
-        if sh('xcode-select -p') == '':
-            fatal("Xcode tools are not installed. Please run xcode-select --install.")
+    def linux_last(self):
+        self.install("valgrind")
 
+    def macos(self):
         self.install_gnu_utils()
-        self.install("cmake")
         self.install("git-lfs")
         self.install("redis")
 
     def common_last(self):
-        self.run("python3 -m pip uninstall -y ramp-packer RLTest || true")
-        # redis-py-cluster should be installed from git due to redis-py dependency
-        self.pip_install("--no-cache-dir git+https://github.com/Grokzen/redis-py-cluster.git@master")
-        self.pip_install("--no-cache-dir git+https://github.com/RedisLabsModules/RLTest.git@master")
-        self.pip_install("--no-cache-dir git+https://github.com/RedisLabs/RAMP@master")
+        self.run("%s/bin/getclang --format" % READIES)
+        if self.platform == "arm":
+            self.run("%s/bin/getcmake" % READIES)
+        else:
+            self.run("%s/bin/getcmake --no-repo" % READIES)
 
-        self.pip_install("-r %s/readies/paella/requirements.txt" % HERE)
+        self.run("{PYTHON} {READIES}/bin/getrmpytools".format(PYTHON=self.python, READIES=READIES))
+
         self.pip_install("-r %s/test/test_requirements.txt" % ROOT)
 
         self.pip_install("awscli")
