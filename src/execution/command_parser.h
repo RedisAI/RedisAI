de369ff8acbb845b5399ec4766493923ef2b369f
DvirDukhan <dvir@redislabs.com>
2021-05-27 23:34:04 +0300

    added command_parser.h

---
+++ src/execution/command_parser.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "redismodule.h"
+#include "run_info.h"
+
+typedef enum RunCommand {
+    CMD_MODELRUN = 0,
+    CMD_SCRIPTRUN,
+    CMD_DAGRUN,
+    CMD_MODELEXECUTE,
+    CMD_SCRIPTEXECUTE,
+    CMD_DAGEXECUTE
+} RunCommand;
+
+/**
+ * @brief  Parse and execute RedisAI run command. After parsing and validation, the resulted
+ * runInfo (DAG) is queued and the client is blocked until the execution is complete (async
+ * execution).
+ * @return Returns REDISMODULE_OK if the command is valid, REDISMODULE_ERR otherwise.
+ */
+int RedisAI_ExecuteCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc,
+                           RunCommand command, bool ro_dag);
